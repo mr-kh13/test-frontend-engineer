a1def678ac48b4bf877fb6e89fb9048c99f525cf
Mohammad <Mohammad.kh06@gmail.com>
2025-01-14 12:18:53 +1100

    Fix header aligment

--- src/core/components/MainLayout/Header/Header.tsx
+++ src/core/components/MainLayout/Header/Header.tsx
@@ -2,8 +2,8 @@ import { CartButton } from "./CartButton";
 
 export function Header() {
   return (
-    <header className="w-full h-16 flex items-center border-b">
-      <div className="container mx-auto px-4 flex items-center">
+    <header className="w-full h-16 flex px-4 items-center border-b">
+      <div className="container mx-auto flex items-center">
         <h1 className="text-xl font-semibold">My Shop!</h1>
         <CartButton />
       </div>
--- src/core/components/MainLayout/Main/Main.tsx
+++ src/core/components/MainLayout/Main/Main.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export function Main({ children }: Props) {
-  return <main className="w-full flex flex-col py-8 p-4">{children}</main>;
+  return <main className="w-full flex flex-col py-8 px-4">{children}</main>;
 }
